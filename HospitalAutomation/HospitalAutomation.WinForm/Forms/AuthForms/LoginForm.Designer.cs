cc3577d773496c20e5b80f263f8527c4e8f75c68
Murat ACAR <muratacar@valfsel.com.tr>
2023-09-01 23:55:41 +0300

    LoginForm Eklendi

--- HospitalAutomation/HospitalAutomation.WinForm/Forms/AuthForms/LoginForm.Designer.cs
+++ HospitalAutomation/HospitalAutomation.WinForm/Forms/AuthForms/LoginForm.Designer.cs
@@ -28,32 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            txtUsername = new TextBox();
+            btnLogin = new Button();
+            label2 = new Label();
+            txtPassword = new TextBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // pictureBox1
             // 
-            button1.Location = new Point(299, 282);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            pictureBox1.Dock = DockStyle.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(619, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(380, 383);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(97, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 36);
+            label1.TabIndex = 1;
+            label1.Text = "Giriş Yapınız";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(214, 149);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(277, 27);
+            txtUsername.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.Maroon;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(339, 246);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(152, 39);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "Giriş Yap";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(97, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Kullanıcı Adı :";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(214, 194);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(277, 27);
+            txtPassword.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(159, 197);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Şifre :";
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.White;
+            ClientSize = new Size(999, 383);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4);
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox txtUsername;
+        private Button btnLogin;
+        private Label label2;
+        private TextBox txtPassword;
+        private Label label3;
     }
 }
