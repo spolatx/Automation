5a2b7a822f1b0c350eaf7b42e337c6d3c817ac53
Murat ACAR <muratacar@valfsel.com.tr>
2023-09-02 17:00:16 +0300

    programcs düzenlemeri yapıldı

--- HospitalAutomation/HospitalAutomation.WinForm/Program.cs
+++ HospitalAutomation/HospitalAutomation.WinForm/Program.cs
@@ -14,7 +14,7 @@ namespace HospitalAutomation.WinForm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new HastaKabulForm());
+            Application.Run(new NavigationForm());
         }
     }
 }
