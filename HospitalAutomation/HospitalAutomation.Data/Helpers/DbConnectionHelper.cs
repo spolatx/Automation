b3c60c36b252862e1aa0705160fe064b8a967e35
Samet Polat <spolat953@gmail.com>
2023-09-02 19:43:31 +0300

    data kısmında repolar ve interfaceler oluşturulmaya başlandı.

---
+++ HospitalAutomation/HospitalAutomation.Data/Helpers/DbConnectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalAutomation.Data.Helpers
+{
+    internal class DbConnectionHelper
+    {
+        public SqlConnection Connection => GetConnection();
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection("server=.\\SQLExpress; database=StudentDb; integrated security=true;");
+        }
+
+    }
+}
