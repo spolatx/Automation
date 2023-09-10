345d9a2d3254397d2ba814cb254b664a73cb18e1
muratacarr <acarrmuratt@gmail.com>
2023-09-10 12:40:38 +0300

    Revert "hasta bilgileri formu düzenlendi"
    This reverts commit f100fc4e5065f427b085835f2020b678dabe6b6f.

--- HospitalAutomation/HospitalAutomation.WinForm/Forms/YoneticiForms/YoneticiHastalarForm.cs
+++ HospitalAutomation/HospitalAutomation.WinForm/Forms/YoneticiForms/YoneticiHastalarForm.cs
@@ -21,10 +21,5 @@ namespace HospitalAutomation.WinForm.Forms.YoneticiForms
         {
             this.Dispose();
         }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
     }
 }
